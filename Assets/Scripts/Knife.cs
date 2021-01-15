bd5c4394a8a60912c39588ed21aa6df9dd229890
holylighto <forceoffear1@gmail.com>
2021-01-15 17:25:03 +0300

    wood destruction
    added:
    wood destruction
    game ended now

--- Assets/Scripts/Knife.cs
+++ Assets/Scripts/Knife.cs
@@ -7,6 +7,7 @@ public class Knife : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private BoxCollider _boxCollider;
+    private bool _isScoreTaken = false;
 
     private void Start()
     {
@@ -27,16 +28,26 @@ public class Knife : MonoBehaviour
         Debug.Log("throw");
     }
 
+    public void EnableGravity()
+    {
+        _rigidbody.useGravity = true;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Wood"))
+        if (collision.gameObject.CompareTag("Wood") && _isScoreTaken == false)
         {
             gameObject.transform.SetParent(collision.transform);
             Vibration.Vibrate(100);
+            var temp = collision.gameObject.GetComponentInParent<Wood>();
+            temp.MinusScore();
+            //_rigidbody.isKinematic = true;
+            _isScoreTaken = true;
+            
         }
-        if(collision.gameObject.CompareTag("Knife"))
+        if(collision.gameObject.CompareTag("Knife") && _isScoreTaken == false)
         {
-            Vibration.Vibrate(100);
+            Vibration.Vibrate();
             SceneManager.LoadScene(0);
         }
     }
--- Assets/Scripts/Wood.cs
+++ Assets/Scripts/Wood.cs
@@ -1,12 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Wood : MonoBehaviour
 {
+    [SerializeField] private float _radius;
+    [SerializeField] private float _power;
+    private Rigidbody[] _rb;
+    [SerializeField]private Transform _explosionPosition;
+    private bool _isGameEnded = false;
+    [SerializeField]private int ScoreToFinish = 5;
+    private LayerMask _mask;
+
 
     private void Update()
     {
-        transform.rotation *= Quaternion.Euler(0, 2, 0);
+        if(!_isGameEnded) transform.rotation *= Quaternion.Euler(0, 2, 0);
+    }
+
+    private void Start()
+    {
+        _explosionPosition = transform;
+        _rb = GetComponentsInChildren<Rigidbody>();
+        _mask = LayerMask.NameToLayer("Wood");
+    }
+
+    private void EndGame()
+    {
+        _isGameEnded = !_isGameEnded;
+        foreach (var a in _rb)
+        {
+            a.isKinematic = false;
+            a.useGravity = true;
+        }
+        var b = FindObjectsOfType<Knife>();
+        foreach(var a in b)
+        {
+            a.EnableGravity();
+        }
+        Collider[] colliders = Physics.OverlapSphere(_explosionPosition.position, _radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            //if (rb != null) rb.AddExplosionForce(_power, _explosionPosition.position, _radius, 3.0f);
+            if (rb != null) rb.AddForce((hit.transform.position - _explosionPosition.position) * _power, ForceMode.Impulse);
+        }
+        Invoke("ReloadScene", 5);
+    }
+
+    public void MinusScore()
+    {
+        if (ScoreToFinish > 0) ScoreToFinish--;
+        if (ScoreToFinish <= 0) EndGame();
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
